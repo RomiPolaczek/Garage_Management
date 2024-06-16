d20d2783592dadfc0030b8f7f0f7417367401514
romipolaczek <168196934+romipolaczek@users.noreply.github.com>
2024-06-17 00:37:37 +0300

    add validation input check, Owner class and status in Garage

--- GarageLogic/FuelEngine.cs
+++ GarageLogic/FuelEngine.cs
@@ -2,6 +2,7 @@ namespace GarageLogic;
 
 public class FuelEngine : PowerUnit
 {
+    private eFuelType m_FuelType;
     public enum eFuelType
     {
         Soler,
@@ -13,6 +14,6 @@ public class FuelEngine : PowerUnit
     public FuelEngine (float i_MaxEnergyCapacity, eFuelType i_FuelType) :
         base(i_MaxEnergyCapacity)
     {
-
+        m_FuelType = i_FuelType;
     }
 }
